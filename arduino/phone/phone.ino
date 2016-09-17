2e8952ad0f4b061d2dc5dcf044fbac7d3475a2cc
burnsed <rburns@stic.net>
2016-09-17 14:57:37 -0700

    Move experiments into tests dir. Add logging to phones

--- arduino/phone/phone.ino
+++ arduino/phone/phone.ino
@@ -1,22 +1,67 @@
-#include <SPI.h>
-#include <SD.h> // preprocessor directives (header files)
+// Create phone
+
+// used by TMRpcm.h
 #define SDPIN 10
+
+#define SPEAKER_PIN 9
+
+
+#include <SPI.h>
+#include <SD.h>
+// https://github.com/TMRh20/TMRpcm
 #include <TMRpcm.h>
 
-TMRpcm tmrpcm;
 
-char mychar; // for reading input from Serial console
+TMRpcm audio;
 
-void setup(){
-  Serial.begin(9600);
-  tmrpcm.speakerPin = 9; //11 on Mega, 9 on Uno, Nano, etc
-  tmrpcm.setVolume(6);
-  if (!SD.begin(SDPIN)) {
-    Serial.println("initialization failed!");
-    return;
-  }
-  Serial.println("playing wav");
-  tmrpcm.play("test4.wav");
+
+void writeLog(String value) {
+    File dataFile = SD.open("log.txt", FILE_WRITE);
+    if (dataFile) {
+        dataFile.println(value);
+        dataFile.close();
+
+        Serial.println("Wrote data");
+    }
+    else {
+        Serial.println("error opening log.txt");
+    }
 }
 
-void loop(){ }
+void setup() {
+    pinMode(SPEAKER_PIN, OUTPUT);
+    digitalWrite(SPEAKER_PIN, LOW);
+
+    Serial.begin(9600);
+    audio.speakerPin = SPEAKER_PIN;
+    audio.setVolume(6);
+    while (true) {
+        if (!SD.begin(SDPIN)) {
+            Serial.println("initialization failed!");
+            writeLog("init failed");
+            delay(100);
+        } else if (!SD.exists("audio.wav")) {
+            writeLog("missing file");
+            delay(1000);
+        } else {
+            break;
+        }
+    }
+
+    Serial.println("playing audio");
+    writeLog("playing");
+    audio.play("audio.wav");
+}
+
+
+void loop() {
+    if (!audio.isPlaying()) {
+        writeLog("done");
+        // wait 10sec then play again
+        delay(10000);
+        writeLog("repeat");
+        audio.play("audio.wav");
+    } else {
+        delay(100);
+    }
+}
---
+++ arduino/tests/logger/logger.ino
@@ -0,0 +1,72 @@
+#include <arduino.h>
+#include <swRTC.h>
+#include <SPI.h>
+#include <SD.h>
+
+// Card select pin for SD card
+#define SDPIN  10
+
+swRTC clock;
+
+String padded(int number) {
+  String tmp = "";
+  if (number > 9) {
+    return tmp + number;
+  }
+  return tmp + "0" + number;
+}
+
+String dateTimeStr(swRTC clk) {
+  String dt = "";
+
+  // 2016-09-16T12:00:00
+  dt += clk.getYear();
+  dt += "-";
+  dt += padded(clk.getMonth());
+  dt += "-";
+  dt += padded(clk.getDay());
+  dt += "T";
+  dt += padded(clk.getHours());
+  dt += ":";
+  dt += padded(clk.getMinutes());
+  dt += ":";
+  dt += padded(clk.getSeconds());
+  return dt;
+}
+
+
+void writeLog(swRTC clock, String value) {
+    File dataFile = SD.open("log.txt", FILE_WRITE);
+    if (dataFile) {
+        dataFile.print(dateTimeStr(clock));
+        dataFile.print(" ");
+        dataFile.println(value);
+        dataFile.close();
+
+        Serial.println("Wrote data");
+    }
+    else {
+        Serial.println("error opening log.txt");
+    }
+}
+
+
+void setup() {
+    clock.stopRTC();
+    clock.setTime(12, 0, 0);
+    clock.setDate(9, 16, 2016);
+    clock.startRTC();
+
+    Serial.begin(9600);
+    if (!SD.begin(SDPIN)) {
+        Serial.println("initialization failed!");
+        return;
+    }
+    Serial.println("SD ready");
+}
+
+
+void loop() {
+    writeLog(dateTimeStr(clock));
+    delay(1000);
+}
