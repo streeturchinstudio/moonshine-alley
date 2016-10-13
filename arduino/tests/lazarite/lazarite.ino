c1687afa02619bb158517258b53313b8719f22dc
burnsed <rburns@stic.net>
2016-10-12 20:51:52 -0700

    Add buttons to adjust frequency

--- arduino/tests/lazarite/lazarite.ino
+++ arduino/tests/lazarite/lazarite.ino
@@ -9,33 +9,36 @@
 #define OUTPUT_PIN  8
 
 // Interblogs say 20 - 50Hz is best. Might tear speaker :-(
-#define FREQUENCY   44
+#define INIT_FREQUENCY   44
 
-#define ENCODER_0   2
-#define ENCODER_1   3
-
-
-// volatile int enc0 = 0;
-// volatile int enc1 = 0;
+#define BTN_0   2
+#define BTN_1   3
 
 
-// void enc0Select() {
-// }
-
-// void enc1Select() {
-// }
+int frequency;
 
 
 void setup() {
-    // attachInterrupt(ENCODER_0, enc0Select, RISING);
-    // attachInterrupt(ENCODER_1, enc1Select, RISING);
+    pinMode(BTN_0, INPUT);
+    pinMode(BTN_1, INPUT);
+
+    digitalWrite(BTN_0, HIGH);
+    digitalWrite(BTN_1, HIGH);
 
     // set a fixed tone
+    frequency = INIT_FREQUENCY;
     pinMode(OUTPUT_PIN, OUTPUT);
-    tone(OUTPUT_PIN, FREQUENCY);
+    tone(OUTPUT_PIN, frequency);
 }
 
 
 void loop() {
-
+    if (digitalRead(BTN_0) == LOW) {
+        frequency += 1;
+        tone(OUTPUT_PIN, frequency);
+    } else if (digitalRead(BTN_1) == LOW) {
+        frequency -= 1;
+        tone(OUTPUT_PIN, frequency);
+    }
+    delay(250);
 }
