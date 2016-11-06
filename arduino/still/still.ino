721715ada7e3aecdad20f0c3e0e4582febe7ac14
burnsed <rburns@stic.net>
2016-11-05 17:42:13 -0700

    Add brightness controls for the fire

--- arduino/still/still.ino
+++ arduino/still/still.ino
@@ -6,13 +6,20 @@
 #define PIN   2
 #define LEDS  24
 
+#define INPUT1  4
+#define INPUT2  5
+
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
+uint32_t off = strip.Color(0, 0, 0);
 int brightness;
 String buffer;
 
 
 void setup() {
+    pinMode(INPUT1, INPUT);
+    pinMode(INPUT2, INPUT);
+
     strip.begin();
     strip.setBrightness(255);
     strip.show();
@@ -24,6 +31,23 @@ void setup() {
     Serial.println("type in a new brightness level (1-256)...");
 }
 
+
+void readBrightness() {
+    int a = digitalRead(INPUT1);
+    int b = digitalRead(INPUT2);
+
+    if (a == LOW && b == LOW) {
+        brightness = 0;
+    } else if (a == LOW && b == HIGH) {
+        brightness = 40;
+    } else if (a == HIGH && b == LOW) {
+        brightness = 80;
+    } else if (a == HIGH && b == HIGH) {
+        brightness = 127;
+    }
+}
+
+
 void loop() {
     if (Serial.available()) {
         char c = Serial.read();
@@ -33,6 +57,16 @@ void loop() {
         } else if (c >= '0' && c <= '9') {
             buffer += c;
         }
+    }
+
+    readBrightness();
+    if (brightness == 0) {
+        for (int x=0; x< LEDS; x++) {
+            strip.setPixelColor(x, off);
+        }
+        strip.show();
+        delay(200);
+        return;
     }
 
     int r1 = min(255, brightness + random(brightness/2));
